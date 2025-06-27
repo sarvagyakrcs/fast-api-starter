d6a0bd38abf0a86e7df7c97e1058bbdb370b63b3
Sarvagya Kumar <sarvagyakrcs@gmail.com>
2025-06-27 09:56:58 +0530

    feat: add Prisma integration and update project dependencies

--- app/main.py
+++ app/main.py
@@ -1,3 +1,19 @@
-from app.utils.load_env import env
+import asyncio
+from prisma import Prisma
 
-print(env.get_env_variable("DATABASE_URL"))
+async def main() -> None:
+    prisma = Prisma()
+    await prisma.connect()
+
+    # write your queries here
+    user = await prisma.user.create(
+        data={
+            'email': 'robert@craigisadasdasde.dev',
+            'password': 'secure_password123'
+        }
+    )
+
+    await prisma.disconnect()
+
+if __name__ == '__main__':
+    asyncio.run(main())
